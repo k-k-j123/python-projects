4aac276c67ffd8df549f1dc1ca0c53e3b31a99c5
Kaushik Joshi <76639214+k-k-j123@users.noreply.github.com>
2024-06-14 15:52:45 +0530

    Update birthday_wisher.py

--- birthday_wisher.py
+++ birthday_wisher.py
@@ -4,7 +4,7 @@ def birthday_greeting(name):
     return f"Happiest Birthday to you {name},\nMay you live a thousnad years \nnote:this is an automated message sent using python script written by kaushik joshi (he wanted to flex his coding skills)"
 
 def send_message(message,when,phone):
-    time_obj = datetime.strptime(when, "%H%M")  # (%H for 24-hour format)
+    time_obj = datetime.strptime(when, "%H%M")  
     send_hr = time_obj.hour
     send_min = time_obj.minute
     kit.sendwhatmsg(phone,message,send_hr,send_min)
